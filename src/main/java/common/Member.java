80ccb5a262165ee7e0a5882e37129fbf18e54de4
simjunbo <s860620@gmail.com>
2019-06-12 14:01:05 +0900

    chapter4

--- src/main/java/common/Member.java
+++ src/main/java/common/Member.java
@@ -2,16 +2,13 @@ package common;
 
 import org.hibernate.annotations.DynamicUpdate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @DynamicUpdate
 @Table(name = "MEMBER")
 public class Member {
-
 	@Id
 	@Column(name = "ID")
 	private String id;
@@ -20,6 +17,18 @@ public class Member {
 	private String username;
 
 	private Integer age;
+
+	@Enumerated(EnumType.STRING)
+	private RoleType type;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastModifiedDate;
+
+	@Lob
+	private String description;
 
 	public String getId() {
 		return id;
@@ -43,5 +52,37 @@ public class Member {
 
 	public void setAge(Integer age) {
 		this.age = age;
+	}
+
+	public RoleType getType() {
+		return type;
+	}
+
+	public void setType(RoleType type) {
+		this.type = type;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 }
---
+++ src/main/java/common/RoleType.java
@@ -0,0 +1,5 @@
+package common;
+
+public enum RoleType {
+	ADMIN, USER
+}
