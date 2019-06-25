e59452795d190c56eeeb4a393ea0f5ca71d7d9d5
simjunbo <s860620@gmail.com>
2019-06-25 13:30:24 +0900

    chapter7

---
+++ src/main/java/chapter8/lazyloading/Member.java
@@ -0,0 +1,21 @@
+package chapter8.lazyloading;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Member {
+
+	@Id
+	private String id;
+	private String username;
+	private Integer age;
+
+	// 팀과 사용자 관계는 많이 사용하기 때문에 즉시로딩
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Team team;
+
+	// 사용자와 주문 관계는 족므 사용하기 때문에 지연로딩
+	@OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+	private List<Order> orders;
+}
---
+++ src/main/java/chapter8/lazyloading/Order.java
@@ -0,0 +1,17 @@
+package chapter8.lazyloading;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Order {
+	private String id;
+
+	@Id public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
